e53cf1f4c38af730d6247d63d317946b5ab976f5
Istiak Hossain <istiakhossainofficial@gmail.com>
2026-07-28 16:25:14 +0600

    I have delete safe unnasar files

--- remove_styles.py
+++
@@ -1,9 +0,0 @@
-﻿import sys
-path = 'c:/Code/MisLoan/resources/js/pages/Member/LoanApplications/Forms/LoanApplicationApprovalPrint.tsx'
-with open(path, 'r', encoding='utf-8') as f:
-    content = f.read()
-content = content.replace(" style={{ fontSize: '12px' }}", '')
-content = content.replace(" style={{ fontSize: '11px' }}", '')
-with open(path, 'w', encoding='utf-8') as f:
-    f.write(content)
-print('Done')
